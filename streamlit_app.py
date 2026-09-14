19bd02f646eb6da4a2a332dcc542dc70518744e4
jaysterling11 <167261997+jaysterling11@users.noreply.github.com>
2026-09-14 00:24:18 +0000

    fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ homework1_page = st.Page("HW1.py", title="Homework 1 - Create The App", icon="1�
 homework2_page = st.Page("HW2.py", title="Homework 2 - URL Summarizer with Multiple LLMs", icon="2️⃣", default=True)
 homework3_page = st.Page("HW3.py", title="Homework 3 - Chatbot with Memory and Context", icon="3️⃣")
 
-pg = st.navigation([homework1_page, homework2_page, homework3_page], title="Homework Navigation", hide_menu=True)
+pg = st.navigation([homework1_page, homework2_page, homework3_page])
 pg.run()
